88e485370c59c402171e5e36f65de785d38e4497
Kulunu Abeysinghe <kalzter911@gmail.com>
2018-12-25 15:26:22 +0530

    moved the navbar component to the core directory

--- Project - Web/src/app/app.module.ts
+++ Project - Web/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import { LoginComponent} from './modules/login/login.component';
 
 @NgModule({
