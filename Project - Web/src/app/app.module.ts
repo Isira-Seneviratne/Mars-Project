be9b488c8d03e591e31ac55efc3d0e8361fe1252
Kulunu Abeysinghe <kalzter911@gmail.com>
2018-12-26 11:50:45 +0530

    Converted Gnanod's interface to an angular project

--- Project - Web/src/app/app.module.ts
+++ Project - Web/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
 import { LoginComponent} from './modules/login/login.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './modules/home/home.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
